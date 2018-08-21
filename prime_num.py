bc186ab040af1b6b2d51ea1297fbbb8c1ec6732b
Alex <alex.wei.0661@gmail.com>
2018-08-21 19:26:30 +0800

    finish is_prime function

--- prime_num.py
+++ prime_num.py
@@ -1,1 +1,9 @@
-print("hello prime")
+def is_prime(num):
+    for p in range(2,num):
+        remainder=num%p
+        if remainder is 0:
+            return False
+        else:
+            continue
+    return True
+print(is_prime(10))
