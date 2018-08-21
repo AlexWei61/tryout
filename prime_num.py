1ba999dbe6ce32214bc4bc7e9118af491ab38c01
Alex <alex.wei.0661@gmail.com>
2018-08-21 19:46:25 +0800

    generate prime number set

--- prime_num.py
+++ prime_num.py
@@ -1,5 +1,5 @@
 def is_prime(num):
-    for p in range(2,num):
+    for p in range(2,int(num/2+1)):
         remainder=num%p
         if remainder is 0:
             return False
@@ -7,3 +7,11 @@ def is_prime(num):
             continue
     return True
 print(is_prime(10))
+
+prime_nums=[]
+max_num=100
+for num in range(2,max_num+1):
+    if is_prime(num):
+        prime_nums.append(num)
+print(prime_nums)
+print(len(prime_nums))
