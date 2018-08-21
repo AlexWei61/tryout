f3a7a1adae031ef84e2da71b24f41afa81405056
Alex <alex.wei.0661@gmail.com>
2018-08-21 22:58:39 +0800

    define a generate primes function, and use main function to test it

--- prime_num.py
+++ prime_num.py
@@ -9,15 +9,21 @@ def is_prime(num, prime_num):
         #    continue
     return True
 
-start = time.time()
-prime_nums=[]
-maxnum=1000000
-for num in range(2,maxnum+1):
-    if is_prime(num, prime_nums):
-        prime_nums.append(num)
-end=time.time()
-print(prime_nums)
-print(len(prime_nums))
-print(end-start)
+def generate_primes(maxnum):
+    start = time.time()
+    prime_nums=[]
+    #maxnum=1000
+    for num in range(2,maxnum+1):
+        if is_prime(num, prime_nums):
+            prime_nums.append(num)
+    end = time.time()
+    print("Generate primes under %d, use time: %f" %(maxnum, end - start))
+    return prime_nums
+
+if __name__ == "__main__":
+    prime_nums = generate_primes(100000)
+    print(prime_nums)
+    print(len(prime_nums))
+
 
 
