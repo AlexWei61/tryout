84049583429f3551654de1c2af5ffe4eac3eb779
Alex <alex.wei.0661@gmail.com>
2018-09-01 18:56:06 +0800

    finish two functions: 1. get_median_grade 2. get_average_grade

--- math.py
+++ math.py
@@ -23,6 +23,54 @@ month_coin = gen_month_coin(12)
 print(month_coin)
 print(len(month_coin))
 
+age= 67
+xxx = 30>=age and age>20
+print(xxx)
+yyy = 10
+
+if 30>=age and age>20:
+    print("you are old")
+    print("another line")
+elif age>30:
+    print("you are too old")
+else:
+    print("you are young")
+max_month = 1
+
+age = 10
+age1 = 5
+
+if age is age1:
+    print("they are same")
+else:
+    print("they are dddd")
 
 
+g1 = [89,78,69,89,96,45,67,31,95,100,32]
+g2 = [89,78,69,89,96,45,67,31,95,100,32,75]
+def get_median_grade(grade):
+    median_grade = 0
+    grade.sort()
+    people_num = len(grade)
+    print(grade)
+    half_num = int(people_num/2)
+    if people_num%2==1:
+        median_grade = grade[half_num]
+    else:
+        m1=grade[half_num-1]
+        m2=grade[half_num]
+        median_grade = ((m1+m2)/2)
+    return (median_grade)
+median_num = get_median_grade(grade=g2)
+print(median_num)
+
+
+def get_average_grade(grade):
+    average = 0
+    for g in grade:
+        average = average+g
+    average = average/len(grade)
+    return average
+average = get_average_grade(grade=g2)
+print(average)
 
